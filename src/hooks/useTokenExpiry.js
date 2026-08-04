89b3f1494f29ba37bc7e52dc616e257b2d7552a1
Shabnam <shabnam.ofl@gmail.com>
2026-08-04 23:40:01 +0530

    Fix: fix-token-expiry-logger

--- src/hooks/useTokenExpiry.js
+++ src/hooks/useTokenExpiry.js
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useCallback } from "react";
 import { toast } from "react-toastify";
 import { isTokenValid, decodeTokenPayload } from "../utils/tokenUtils";
-import { syncSecureStorage } from "../utils/secureStorage";
+import { syncSecureStorage } from "../utils/secureStorage.js";
+import { logger } from "../utils/logger.js";
 import { logger } from "../utils/logger";
 export const MAX_TOKEN_EXPIRY_TIMEOUT_MS = 2_147_483_647;
 const TOKEN_EXPIRY_BUFFER_MS = 1_000;
